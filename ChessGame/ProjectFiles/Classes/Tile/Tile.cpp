46ef75a2138f0c26223864635668fb1ba9f9105e
Go4Real34 <gorkemsarikaya2017@gmail.com>
2023-11-16 23:07:25 +0300

    add Tile class function implementations in source file

--- ChessGame/ProjectFiles/Classes/Tile/Tile.cpp
+++ ChessGame/ProjectFiles/Classes/Tile/Tile.cpp
@@ -1,1 +1,18 @@
 #include "Tile.h"
+
+Tile::Tile(bool& IsBackgroundWhite, Piece& HeldPiece) {
+	this -> isBackgroundWhite = IsBackgroundWhite;
+	this -> heldPiece = &HeldPiece;
+}
+
+Tile::~Tile() {
+	delete this -> heldPiece;
+}
+
+bool Tile::getIsBackgroundWhite() const {
+	return this -> isBackgroundWhite;
+}
+
+Piece* Tile::getHeldPiece() const {
+	return this -> heldPiece;
+}
