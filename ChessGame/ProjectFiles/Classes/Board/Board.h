bd0c95e1be988621cb84bf77daeeecffe4496610
Go4Real34 <gorkemsarikaya2017@gmail.com>
2023-11-17 01:28:01 +0300

    add necessary Board class fields in header file

--- ChessGame/ProjectFiles/Classes/Board/Board.h
+++ ChessGame/ProjectFiles/Classes/Board/Board.h
@@ -1,4 +1,22 @@
 #pragma once
 
+#include "../Tile/Tile.h"
+
 class Board {
+	private:
+		static const uint8_t BOARD_LENGTH = 8;
+
+	public:
+		Board();
+		~Board();
+
+		uint8_t getBoardLength() const;
+		
+		struct BoardTile {
+			Tile tiles[BOARD_LENGTH][BOARD_LENGTH];
+		};
+		BoardTile getBoard() const;
+
+	private:
+		BoardTile board;
 };
