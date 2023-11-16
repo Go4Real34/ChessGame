cfb5a744230ed6ae0ee00fdbfdd2681079b74543
Go4Real34 <gorkemsarikaya2017@gmail.com>
2023-11-16 22:51:04 +0300

    add base Piece class getter function implementations in source file

--- ChessGame/ProjectFiles/Classes/Piece/Piece.cpp
+++ ChessGame/ProjectFiles/Classes/Piece/Piece.cpp
@@ -1,1 +1,9 @@
 #include "Piece.h"
+
+bool Piece::getIsWhiteColor() const {
+	return this -> isWhiteColor;
+}
+
+std::vector<std::string> Piece::getPossibleMoves() const {
+	return this -> possibleMoves;
+}
