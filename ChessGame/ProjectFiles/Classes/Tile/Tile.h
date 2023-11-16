5fad8324ce045cdae9c7aef621e552b9e45730fd
Go4Real34 <gorkemsarikaya2017@gmail.com>
2023-11-16 23:07:13 +0300

    add necessary Tile class fields in header file

--- ChessGame/ProjectFiles/Classes/Tile/Tile.h
+++ ChessGame/ProjectFiles/Classes/Tile/Tile.h
@@ -1,4 +1,16 @@
 #pragma once
 
+#include "../Piece/Piece.h"
+
 class Tile {
+	public:
+		Tile(bool& IsBackgroundWhite, Piece& Piece);
+		~Tile();
+
+		bool getIsBackgroundWhite() const;
+		Piece* getHeldPiece() const;
+
+	private:
+		bool isBackgroundWhite = false;
+		Piece* heldPiece = nullptr;
 };
