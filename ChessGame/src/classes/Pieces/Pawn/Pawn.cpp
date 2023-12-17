0d0ee5b8c9c09a7beb0966b58dda70851f38b20e
Go4Real34 <gorkemsarikaya2017@gmail.com>
2023-12-17 23:36:22 +0300

    add Pawn class initialization function implementations

--- ChessGame/src/classes/Pieces/Pawn/Pawn.cpp
+++ ChessGame/src/classes/Pieces/Pawn/Pawn.cpp
@@ -1,1 +1,21 @@
+#include <SFML/Graphics.hpp>
+
 #include "Pawn.h"
+
+Pawn::Pawn(bool IsWhite, int8_t XCoord, int8_t YCoord) {
+	this -> isWhite = IsWhite;
+	this -> xCoord = XCoord;
+	this -> yCoord = YCoord;
+
+	sf::Texture texture;
+	sf::IntRect blankPieceBackground;
+	if (this -> isWhite) {
+		texture.loadFromFile("./assets/textures/white_pawn.png", blankPieceBackground);
+	} else {
+		texture.loadFromFile("./assets/textures/black_pawn.png", blankPieceBackground);
+	}
+	this -> sprite.setTexture(texture, true);
+};
+
+Pawn::~Pawn() {
+}
