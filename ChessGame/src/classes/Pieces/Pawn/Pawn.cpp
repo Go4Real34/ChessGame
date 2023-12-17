0ef1eeaad6798b34a5f7d99a88482260cb005f28
Go4Real34 <gorkemsarikaya2017@gmail.com>
2023-12-17 23:47:33 +0300

    add Pawn class legal move calculator function implementation

--- ChessGame/src/classes/Pieces/Pawn/Pawn.cpp
+++ ChessGame/src/classes/Pieces/Pawn/Pawn.cpp
@@ -19,3 +19,102 @@ Pawn::Pawn(bool IsWhite, int8_t XCoord, int8_t YCoord) {
 
 Pawn::~Pawn() {
 }
+
+void Pawn::getPieceLegalMoves(std::vector<std::vector<Piece*>>& board, std::vector<Move>& allMoves, int8_t xCoord, int8_t yCoord) {
+	if (!isPiecePawn(board, xCoord, yCoord)) {
+		return;
+	}
+
+	if (!isInVerticalBorder(yCoord)) {
+		return;
+	}
+
+	if (isFrontEmpty(board, xCoord, yCoord)) {
+		if (this -> isWhite) {
+			allMoves.push_back(Move(xCoord, yCoord, xCoord, yCoord - 1));
+		} else {
+			allMoves.push_back(Move(xCoord, yCoord, xCoord, yCoord + 1));
+		}
+
+		if (isAtStartingPosition(board, xCoord, yCoord)) {
+			if (this -> isWhite) {
+				allMoves.push_back(Move(xCoord, yCoord, xCoord, yCoord - 2));
+			} else {
+				allMoves.push_back(Move(xCoord, yCoord, xCoord, yCoord + 2));
+			}
+		}
+	}
+
+	if (isAtRightOfLeftBorder(yCoord)) {
+		if (isEnemyPieceAtLeftDiagonal(board, xCoord, yCoord)) {
+			if (this -> isWhite) {
+				allMoves.push_back(Move(xCoord, yCoord, xCoord - 1, yCoord - 1));
+			} else {
+				allMoves.push_back(Move(xCoord, yCoord, xCoord - 1, yCoord + 1));
+			}
+		}
+	}
+	
+	if (isAtLeftOfRightBorder(yCoord)) {
+		if (isEnemyPieceAtRightDiagonal(board, xCoord, yCoord)) {
+			if (this -> isWhite) {
+				allMoves.push_back(Move(xCoord, yCoord, xCoord + 1, yCoord - 1));
+			} else {
+				allMoves.push_back(Move(xCoord, yCoord, xCoord + 1, yCoord + 1));
+			}
+		}
+	}
+}
+
+
+bool Pawn::isPiecePawn(std::vector<std::vector<Piece*>>& board, int8_t xCoord, int8_t yCoord) {
+	return dynamic_cast<Pawn*>(board[xCoord][yCoord]) != nullptr;
+}
+
+bool Pawn::isInVerticalBorder(int8_t yCoord) {
+	if (this -> isWhite) {
+		return yCoord > 0;
+	} else {
+		return yCoord < 7;
+	}
+}
+
+bool Pawn::isFrontEmpty(std::vector<std::vector<Piece*>>& board, int8_t xCoord, int8_t yCoord) {
+	if (this -> isWhite) {
+		return dynamic_cast<Piece*>(board[xCoord][yCoord - 1]) == nullptr;
+	} else {
+		return dynamic_cast<Piece*>(board[xCoord][yCoord + 1]) == nullptr;
+	}
+}
+
+bool Pawn::isAtStartingPosition(std::vector<std::vector<Piece*>>& board, int8_t xCoord, int8_t yCoord) {
+	if (this -> isWhite) {
+		return dynamic_cast<Piece*>(board[xCoord][yCoord - 2]) == nullptr && yCoord == 6;
+	} else {
+		return dynamic_cast<Piece*>(board[xCoord][yCoord + 2]) == nullptr && yCoord == 1;
+	}
+}
+
+bool Pawn::isAtRightOfLeftBorder(int8_t xCoord) {
+	return xCoord > 0;
+}
+
+bool Pawn::isEnemyPieceAtLeftDiagonal(std::vector<std::vector<Piece*>>& board, int8_t xCoord, int8_t yCoord) {
+	if (this -> isWhite) {
+		return (dynamic_cast<Piece*>(board[xCoord - 1][yCoord - 1]) != nullptr && !(board[xCoord - 1][yCoord - 1] -> getIsWhite()));
+	} else {
+		return (dynamic_cast<Piece*>(board[xCoord - 1][yCoord + 1]) != nullptr && board[xCoord - 1][yCoord + 1] -> getIsWhite());
+	}
+}
+
+bool Pawn::isAtLeftOfRightBorder(int8_t xCoord) {
+	return xCoord < 7;
+}
+
+bool Pawn::isEnemyPieceAtRightDiagonal(std::vector<std::vector<Piece*>>& board, int8_t xCoord, int8_t yCoord) {
+	if (this -> isWhite) {
+		return (dynamic_cast<Piece*>(board[xCoord + 1][yCoord - 1]) != nullptr && !(board[xCoord + 1][yCoord - 1] -> getIsWhite()));
+	} else {
+		return (dynamic_cast<Piece*>(board[xCoord + 1][yCoord + 1]) != nullptr && board[xCoord + 1][yCoord + 1] -> getIsWhite());
+	}
+}
--- ChessGame/src/classes/Pieces/Pawn/Pawn.h
+++ ChessGame/src/classes/Pieces/Pawn/Pawn.h
@@ -7,5 +7,19 @@ class Pawn : public Piece {
 		Pawn(bool IsWhite, int8_t XCoord, int8_t YCoord);
 		~Pawn();
 
-		void getPieceLegalMoves(std::vector<std::vector<Piece*>>& board, std::vector<Move>& allMoves, int8_t xCoord, int8_t yCoord);
+		void getPieceLegalMoves(std::vector<std::vector<Piece*>>& board, std::vector<Move>& allMoves, int8_t xCoord, int8_t yCoord) override;
+
+	private:
+		bool isPiecePawn(std::vector<std::vector<Piece*>>& board, int8_t xCoord, int8_t yCoord);
+		
+		bool isInVerticalBorder(int8_t yCoord);
+		
+		bool isFrontEmpty(std::vector<std::vector<Piece*>>& board, int8_t xCoord, int8_t yCoord);
+		bool isAtStartingPosition(std::vector<std::vector<Piece*>>& board, int8_t xCoord, int8_t yCoord);
+		
+		bool isAtRightOfLeftBorder(int8_t xCoord);
+		bool isEnemyPieceAtLeftDiagonal(std::vector<std::vector<Piece*>>& board, int8_t xCoord, int8_t yCoord);
+		
+		bool isAtLeftOfRightBorder(int8_t xCoord);
+		bool isEnemyPieceAtRightDiagonal(std::vector<std::vector<Piece*>>& board, int8_t xCoord, int8_t yCoord);
 };
