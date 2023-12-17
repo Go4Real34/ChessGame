a605fc1d3ac391689e8b8b55281611d3db04b4a5
Go4Real34 <gorkemsarikaya2017@gmail.com>
2023-12-18 00:29:07 +0300

    add Knight class inialization function implementations

--- ChessGame/src/classes/Pieces/Knight/Knight.cpp
+++ ChessGame/src/classes/Pieces/Knight/Knight.cpp
@@ -1,1 +1,21 @@
+#include <SFML/Graphics.hpp>
+
 #include "Knight.h"
+
+Knight::Knight(bool isWhite, int8_t XCoord, int8_t YCoord) {
+	this -> isWhite = isWhite;
+	this -> xCoord = XCoord;
+	this -> yCoord = YCoord;
+
+	sf::Texture texture;
+	sf::IntRect blankPieceBackground;
+	if (this -> isWhite) {
+		texture.loadFromFile("./assets/textures/white_knight.png", blankPieceBackground);
+	} else {
+		texture.loadFromFile("./assets/textures/black_knight.png", blankPieceBackground);
+	}
+	this -> sprite.setTexture(texture, true);
+}
+
+Knight::~Knight() {
+}
