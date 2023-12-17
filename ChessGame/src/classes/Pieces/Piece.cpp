4a58e5fb855f91b63b2d954b17e3a5279c07e6d8
Go4Real34 <gorkemsarikaya2017@gmail.com>
2023-12-17 23:23:01 +0300

    add Piece class function implementations

--- ChessGame/src/classes/Pieces/Piece.cpp
+++ ChessGame/src/classes/Pieces/Piece.cpp
@@ -1,1 +1,17 @@
 #include "Piece.h"
+
+sf::Sprite Piece::getSprite() {
+	return sprite;
+}
+
+bool Piece::getIsWhite() {
+	return isWhite;
+}
+
+int8_t Piece::getXCoord() {
+	return xCoord;
+}
+
+int8_t Piece::getYCoord() {
+	return yCoord;
+}
