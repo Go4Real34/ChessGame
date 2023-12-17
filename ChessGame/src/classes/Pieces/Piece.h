0029115f289b416cf9afa7a3c17a600a40d2a8b7
Go4Real34 <gorkemsarikaya2017@gmail.com>
2023-12-17 18:03:57 +0300

    add Piece class initialization function declarations

--- ChessGame/src/classes/Pieces/Piece.h
+++ ChessGame/src/classes/Pieces/Piece.h
@@ -1,4 +1,32 @@
 #pragma once
 
+#include <iostream>
+#include <vector>
+
+#include <SFML/Graphics.hpp>
+
 class Piece {
+	public:
+		Piece() = default;
+		~Piece() = default;
+		
+		struct Move {
+			int8_t xCoordOld;
+			int8_t xCoordNew;
+			int8_t yCoordOld;
+			int8_t yCoordNew;
+		};
+
+		sf::Sprite getSprite();
+		bool getIsWhite();
+		int8_t getXCoord();
+		int8_t getYCoord();
+		
+		virtual void getPieceLegalMoves(std::vector<Move>& board, std::vector<Move>& allMoves, int8_t xCoord, int8_t yCoord) = 0;
+	
+	protected:
+		sf::Sprite sprite;
+		bool isWhite;
+		int8_t xCoord;
+		int8_t yCoord;
 };
