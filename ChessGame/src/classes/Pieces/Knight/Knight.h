f23ca5e5c55a5e410ee7d485e838a0c8dd72539d
Go4Real34 <gorkemsarikaya2017@gmail.com>
2023-12-18 00:28:34 +0300

    add Knight class initialization function declarations

--- ChessGame/src/classes/Pieces/Knight/Knight.h
+++ ChessGame/src/classes/Pieces/Knight/Knight.h
@@ -3,4 +3,9 @@
 #include "../Piece.h"
 
 class Knight : public Piece {
+	public:
+		Knight(bool isWhite, int8_t XCoord, int8_t YCoord);
+		~Knight();
+
+		void getPieceLegalMoves(std::vector<std::vector<Piece*>>& board, std::vector<Move>& allMoves, int8_t xCoord, int8_t yCoord) override;
 };
