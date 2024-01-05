57a58d2e7a42328c0759d832ce2f5ba25e1f2069
Go4Real34 <gorkemsarikaya2017@gmail.com>
2024-01-05 22:44:04 +0300

    add Window class initialization function implementations

--- ChessGame/src/classes/Window/Window.cpp
+++ ChessGame/src/classes/Window/Window.cpp
@@ -1,1 +1,27 @@
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+
 #include "Window.h"
+
+Window::Window(int width, int height, std::string windowName) {
+	this -> window.create(sf::VideoMode(width, height), windowName);
+}
+
+Window::~Window() {
+}
+
+void Window::run() {
+	while (this -> window.isOpen()) {
+		sf::Event event;
+		if (window.waitEvent(event)) {
+			if (event.type == sf::Event::Closed) {
+				this -> window.close();
+			}
+		}
+
+		this -> window.clear();
+		this -> window.display();
+	}
+}
