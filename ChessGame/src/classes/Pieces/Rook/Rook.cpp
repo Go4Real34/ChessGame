739809c41a4c9fe02f587dc33253d04ca407980b
Go4Real34 <gorkemsarikaya2017@gmail.com>
2024-01-05 15:12:45 +0300

    add Rook class legal move calculator function implementation

--- ChessGame/src/classes/Pieces/Rook/Rook.cpp
+++ ChessGame/src/classes/Pieces/Rook/Rook.cpp
@@ -17,3 +17,120 @@ Rook::Rook(bool isWhite, int8_t XCoord, int8_t YCoord) {
 
 Rook::~Rook() {
 }
+
+void Rook::getPieceLegalMoves(std::vector<std::vector<Piece*>>& board, std::vector<Move>& allMoves, int8_t xCoord, int8_t yCoord) {
+	if (!isPieceRook(board, xCoord, yCoord)) {
+		return;
+	}
+
+	addBottomMoves(board, allMoves, xCoord, yCoord);
+	addTopMoves(board, allMoves, xCoord, yCoord);
+	addLeftMoves(board, allMoves, xCoord, yCoord);
+	addRightMoves(board, allMoves, xCoord, yCoord);
+}
+
+
+bool isPieceRook(std::vector<std::vector<Piece*>>& board, int8_t xCoord, int8_t yCoord) {
+	return (dynamic_cast<Rook*>(board[xCoord][yCoord]));
+}
+
+void Rook::addBottomMoves(std::vector<std::vector<Piece*>>& board, std::vector<Move>& allMoves, int8_t xCoord, int8_t yCoord) {
+	int8_t offsetX = 0;
+	int8_t offsetY = 0;
+	while (true) {
+		offsetX++;
+	
+		if (willBeAtBoundry(xCoord, offsetX, yCoord, offsetY)) {
+			return;
+		}
+	
+		if (isSquareEmpty(board, xCoord + offsetX, yCoord + offsetY)) {
+			allMoves.push_back(Move(xCoord, yCoord, xCoord + offsetX, yCoord + offsetY));
+		} else {
+			return;
+		}
+	}
+}
+
+bool Rook::willBeAtBoundry(int8_t xCoord, int8_t offsetX, int8_t yCoord, int8_t offsetY) {
+	return (willBeAtRightmostColumn(xCoord, offsetX) || willBeAtLeftmostColumn(xCoord, offsetX) || 
+			willBeAtTopmostRow(yCoord, offsetY) || willBeAtBottommostRow(yCoord, offsetY));
+}
+
+bool Rook::willBeAtRightmostColumn(int8_t xCoord, int8_t offsetX) {
+	return (xCoord + offsetX > 7);
+}
+
+bool Rook::willBeAtLeftmostColumn(int8_t xCoord, int8_t offsetX) {
+	return (xCoord + offsetX < 0);
+}
+
+bool Rook::willBeAtTopmostRow(int8_t yCoord, int8_t offsetY) {
+	return (yCoord + offsetY > 7);
+}
+
+bool Rook::willBeAtBottommostRow(int8_t yCoord, int8_t offsetY) {
+	return (yCoord + offsetY < 0);
+}
+
+bool Rook::isSquareEmpty(std::vector<std::vector<Piece*>>& board, int8_t xCoord, int8_t yCoord) {
+	if (this -> isWhite) {
+		return (dynamic_cast<Piece*>(board[xCoord][yCoord]) == nullptr || !(board[xCoord][yCoord] -> getIsWhite()));
+	} else {
+		return (dynamic_cast<Piece*>(board[xCoord][yCoord]) == nullptr || board[xCoord][yCoord] -> getIsWhite());
+	}
+}
+
+void Rook::addTopMoves(std::vector<std::vector<Piece*>>& board, std::vector<Move>& allMoves, int8_t xCoord, int8_t yCoord) {
+	int8_t offsetX = 0;
+	int8_t offsetY = 0;
+	while (true) {
+		offsetX--;
+		
+		if (willBeAtBoundry(xCoord, offsetX, yCoord, offsetY)) {
+			return;
+		}
+		
+		if (isSquareEmpty(board, xCoord + offsetX, yCoord + offsetY)) {
+			allMoves.push_back(Move(xCoord, yCoord, xCoord + offsetX, yCoord + offsetY));
+		} else {
+			return;
+		}
+	}
+}
+
+void Rook::addLeftMoves(std::vector<std::vector<Piece*>>& board, std::vector<Move>& allMoves, int8_t xCoord, int8_t yCoord) {
+	int8_t offsetX = 0;
+	int8_t offsetY = 0;
+	while (true) {
+		offsetY--;
+			
+		if (willBeAtBoundry(xCoord, offsetX, yCoord, offsetY)) {
+			return;
+		}
+			
+		if (isSquareEmpty(board, xCoord + offsetX, yCoord + offsetY)) {
+			allMoves.push_back(Move(xCoord, yCoord, xCoord + offsetX, yCoord + offsetY));
+		} else {
+			return;
+		}
+	}
+}
+
+void Rook::addRightMoves(std::vector<std::vector<Piece*>>& board, std::vector<Move>& allMoves, int8_t xCoord, int8_t yCoord) {
+	int8_t offsetX = 0;
+	int8_t offsetY = 0;
+	while (true) {
+		offsetY++;
+				
+		if (willBeAtBoundry(xCoord, offsetX, yCoord, offsetY)) {
+			return;
+		}
+				
+		if (isSquareEmpty(board, xCoord + offsetX, yCoord + offsetY)) {
+			allMoves.push_back(Move(xCoord, yCoord, xCoord + offsetX, yCoord + offsetY));
+		} else {
+			return;
+		}
+	}
+}
--- ChessGame/src/classes/Pieces/Rook/Rook.h
+++ ChessGame/src/classes/Pieces/Rook/Rook.h
@@ -8,4 +8,21 @@ class Rook : public Piece {
 		~Rook();
 
 		void getPieceLegalMoves(std::vector<std::vector<Piece*>>& board, std::vector<Move>& allMoves, int8_t xCoord, int8_t yCoord) override;
+
+	private:
+		bool isPieceRook(std::vector<std::vector<Piece*>>& board, int8_t xCoord, int8_t yCoord);
+
+		void addBottomMoves(std::vector<std::vector<Piece*>>& board, std::vector<Move>& allMoves, int8_t xCoord, int8_t yCoord);
+		bool willBeAtBoundry(int8_t xCoord, int8_t offsetX, int8_t yCoord, int8_t offsetY);
+		bool willBeAtRightmostColumn(int8_t xCoord, int8_t offsetX);
+		bool willBeAtLeftmostColumn(int8_t xCoord, int8_t offsetX);
+		bool willBeAtTopmostRow(int8_t yCoord, int8_t offsetY);
+		bool willBeAtBottommostRow(int8_t yCoord, int8_t offsetY);
+		bool isSquareEmpty(std::vector<std::vector<Piece*>>& board, int8_t xCoord, int8_t yCoord);
+
+		void addTopMoves(std::vector<std::vector<Piece*>>& board, std::vector<Move>& allMoves, int8_t xCoord, int8_t yCoord);
+	
+		void addLeftMoves(std::vector<std::vector<Piece*>>& board, std::vector<Move>& allMoves, int8_t xCoord, int8_t yCoord);
+	
+		void addRightMoves(std::vector<std::vector<Piece*>>& board, std::vector<Move>& allMoves, int8_t xCoord, int8_t yCoord);
 };
