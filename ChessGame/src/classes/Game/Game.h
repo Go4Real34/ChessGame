94955aa87c5c316841a8894bb52bb32405b8f50d
Go4Real34 <gorkemsarikaya2017@gmail.com>
2024-01-05 15:53:37 +0300

    add Game class initialization function declarations

--- ChessGame/src/classes/Game/Game.h
+++ ChessGame/src/classes/Game/Game.h
@@ -1,4 +1,34 @@
 #pragma once
 
+#include <iostream>
+#include <vector>
+
+#include "../../structs/Move/Move.h"
+
+#include "../Pieces/Pawn/Pawn.h"
+#include "../Pieces/Knight/Knight.h"
+#include "../Pieces/Bishop/Bishop.h"
+#include "../Pieces/Rook/Rook.h"
+#include "../Pieces/Queen/Queen.h"
+#include "../Pieces/King/King.h"
+
 class Game {
+	public:
+		Game();
+		~Game();
+
+		struct Board {
+			Piece* board[8][8];
+		};
+		
+		Board getCurrentBoard();
+		bool playMove(Move move);
+		void skipToNextTurn();
+
+	private:
+		bool isTurnWhites;
+		Board currentBoard;
+		std::vector<Board> boardHistory;
+
+		std::vector<Move> calculateLegalMoves();
 };
