66fb4e0e15eec263585819f2bb4194b98910d91e
Go4Real34 <gorkemsarikaya2017@gmail.com>
2024-01-05 15:02:32 +0300

    add Bishop class legal move calculator function implementation

--- ChessGame/src/classes/Pieces/Bishop/Bishop.h
+++ ChessGame/src/classes/Pieces/Bishop/Bishop.h
@@ -8,4 +8,21 @@ class Bishop : public Piece {
 		~Bishop();
 
 		void getPieceLegalMoves(std::vector<std::vector<Piece*>>& board, std::vector<Move>& allMoves, int8_t xCoord, int8_t yCoord) override;
+
+	private:
+		bool isPieceBishop(std::vector<std::vector<Piece*>>& board, int8_t xCoord, int8_t yCoord);
+
+		void addBottomLeftDiagonalMoves(std::vector<std::vector<Piece*>>& board, std::vector<Move>& allMoves, int8_t xCoord, int8_t yCoord);
+		bool willBeAtBoundry(int8_t xCoord, int8_t offsetX, int8_t yCoord, int8_t offsetY);
+		bool willBeAtRightmostColumn(int8_t xCoord, int8_t offsetX);
+		bool willBeAtLeftmostColumn(int8_t xCoord, int8_t offsetX);
+		bool willBeAtTopmostRow(int8_t yCoord, int8_t offsetY);
+		bool willBeAtBottommostRow(int8_t yCoord, int8_t offsetY);
+		bool isSquareEmpty(std::vector<std::vector<Piece*>>& board, int8_t xCoord, int8_t yCoord);
+
+		void addUpRightDiagonalMoves(std::vector<std::vector<Piece*>>& board, std::vector<Move>& allMoves, int8_t xCoord, int8_t yCoord);
+		
+		void addUpLeftDiagonalMoves(std::vector<std::vector<Piece*>>& board, std::vector<Move>& allMoves, int8_t xCoord, int8_t yCoord);
+		
+		void addBottomRightDiagonalMoves(std::vector<std::vector<Piece*>>& board, std::vector<Move>& allMoves, int8_t xCoord, int8_t yCoord);
 };
