0ab61263e06540b7afb38433f9931bd73d071458
Go4Real34 <gorkemsarikaya2017@gmail.com>
2024-01-05 15:17:21 +0300

    add Queen class initialization function implementations

--- ChessGame/src/classes/Pieces/Queen/Queen.cpp
+++ ChessGame/src/classes/Pieces/Queen/Queen.cpp
@@ -1,1 +1,21 @@
+#include <SFML/Graphics.hpp>
+
 #include "Queen.h"
+
+Queen::Queen(bool isWhite, int8_t XCoord, int8_t YCoord) : Piece(isWhite, XCoord, YCoord) {
+	this -> isWhite = isWhite;
+	this -> xCoord = XCoord;
+	this -> yCoord = YCoord;
+
+	sf::Texture texture;
+	sf::IntRect blankPieceBackground;
+	if (this -> isWhite) {
+		texture.loadFromFile("./assets/textures/white_queen.png", blankPieceBackground);
+	} else {
+		texture.loadFromFile("./assets/textures/black_queen.png", blankPieceBackground);
+	}
+	this -> sprite.setTexture(texture, true);
+}
+
+Queen::~Queen() {
+}
