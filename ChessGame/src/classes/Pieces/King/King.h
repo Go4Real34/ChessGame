b93c9a27f10e18076198ac873c32f7418817d8ba
Go4Real34 <gorkemsarikaya2017@gmail.com>
2024-01-05 15:26:15 +0300

    add King class initialization function declarations

--- ChessGame/src/classes/Pieces/King/King.h
+++ ChessGame/src/classes/Pieces/King/King.h
@@ -3,4 +3,9 @@
 #include "../Piece.h"
 
 class King : public Piece {
+	public:
+		King(bool isWhite, int8_t XCoord, int8_t YCoord);
+		~King();
+
+		void getPieceLegalMoves(std::vector<std::vector<Piece*>>& board, std::vector<Move>& allMoves, int8_t xCoord, int8_t yCoord) override;
 };
