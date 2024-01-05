835dc6c8c719f86903ecc6185c88d65212f3d5e4
Go4Real34 <gorkemsarikaya2017@gmail.com>
2024-01-05 22:43:39 +0300

    add Window class initializaiton functions declarations

--- ChessGame/src/classes/Window/Window.h
+++ ChessGame/src/classes/Window/Window.h
@@ -1,4 +1,19 @@
 #pragma once
 
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio/Sound.hpp>
+#include <SFML/Audio/SoundBuffer.hpp>
+
+#include "../Game/Game.h"
+
 class Window {
+	public:
+		Window(int width, int height, std::string windowName);
+		~Window();
+
+		void run();
+
+	private:
+		sf::RenderWindow window;
 };
