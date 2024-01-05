e35e3d37a5ee353ff0ad01ff3938ef8056b1d497
Go4Real34 <gorkemsarikaya2017@gmail.com>
2024-01-05 15:08:42 +0300

    add Rook class initialization function implementations

--- ChessGame/src/classes/Pieces/Rook/Rook.cpp
+++ ChessGame/src/classes/Pieces/Rook/Rook.cpp
@@ -1,1 +1,19 @@
 #include "Rook.h"
+
+Rook::Rook(bool isWhite, int8_t XCoord, int8_t YCoord) {
+	this -> isWhite = isWhite;
+	this -> xCoord = XCoord;
+	this -> yCoord = YCoord;
+
+	sf::Texture texture;
+	sf::IntRect blankPieceBackground;
+	if (this -> isWhite) {
+		texture.loadFromFile("./assets/textures/white_rook.png", blankPieceBackground);
+	} else {
+		texture.loadFromFile("./assets/textures/black_rook.png", blankPieceBackground);
+	}
+	this -> sprite.setTexture(texture, true);
+}
+
+Rook::~Rook() {
+}
