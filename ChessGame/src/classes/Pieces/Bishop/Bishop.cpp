fd9b8f4f7523d9ecea146f829e2175f12bee3157
Go4Real34 <gorkemsarikaya2017@gmail.com>
2024-01-05 14:18:25 +0300

    add Bishop class initialization function implementations

--- ChessGame/src/classes/Pieces/Bishop/Bishop.cpp
+++ ChessGame/src/classes/Pieces/Bishop/Bishop.cpp
@@ -1,1 +1,21 @@
+#include <SFML/Graphics.hpp>
+
 #include "Bishop.h"
+
+Bishop::Bishop(bool isWhite, int8_t XCoord, int8_t YCoord) {
+	this -> isWhite = isWhite;
+	this -> xCoord = XCoord;
+	this -> yCoord = YCoord;
+
+	sf::Texture texture;
+	sf::IntRect blankPieceBackground;
+	if (this -> isWhite) {
+		texture.loadFromFile("./assets/textures/white_bishop.png", blankPieceBackground);
+	} else {
+		texture.loadFromFile("./assets/textures/black_bishop.png", blankPieceBackground);
+	}
+	this -> sprite.setTexture(texture, true);
+}
+
+Bishop::~Bishop() {
+}
