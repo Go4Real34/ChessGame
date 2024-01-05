a7519b7f2dce39a47750ed64089e440fbda5ea10
Go4Real34 <gorkemsarikaya2017@gmail.com>
2024-01-05 15:42:49 +0300

    add King legal move calculator function implementation

--- ChessGame/src/classes/Pieces/King/King.cpp
+++ ChessGame/src/classes/Pieces/King/King.cpp
@@ -19,3 +19,102 @@ King::King(bool isWhite, int8_t XCoord, int8_t YCoord) {
 
 King::~King() {
 }
+
+void King::getPieceLegalMoves(std::vector<std::vector<Piece*>>& board, std::vector<Move>& allMoves, int8_t xCoord, int8_t yCoord) {
+	if (!isPieceKing(board, xCoord, yCoord)) {
+		return;
+	}
+
+	if (canMoveUp(board, xCoord)) {
+		if (isSquareEmpty(board, xCoord - 1, yCoord)) {
+			allMoves.push_back(Move(xCoord, yCoord, xCoord - 1, yCoord));
+		}
+	}
+
+	if (canMoveUpRightDiagonally(board, xCoord, yCoord)) {
+		if (isSquareEmpty(board, xCoord - 1, yCoord - 1)) {
+			allMoves.push_back(Move(xCoord, yCoord, xCoord - 1, yCoord - 1));
+		}
+	}
+
+	if (canMoveUpLeftDiagonally(board, xCoord, yCoord)) {
+		if (isSquareEmpty(board, xCoord - 1, yCoord + 1)) {
+			allMoves.push_back(Move(xCoord, yCoord, xCoord - 1, yCoord + 1));
+		}
+	}
+
+	if (canMoveRight(board, yCoord)) {
+		if (isSquareEmpty(board, xCoord, yCoord - 1)) {
+			allMoves.push_back(Move(xCoord, yCoord, xCoord, yCoord - 1));
+		}
+	}
+
+	if (canMoveLeft(board, yCoord)) {
+		if (isSquareEmpty(board, xCoord, yCoord + 1)) {
+			allMoves.push_back(Move(xCoord, yCoord, xCoord, yCoord + 1));
+		}
+	}
+
+	if (canMoveDown(board, xCoord)) {
+		if (isSquareEmpty(board, xCoord + 1, yCoord)) {
+			allMoves.push_back(Move(xCoord, yCoord, xCoord + 1, yCoord));
+		}
+	}
+
+	if (canMoveDownLeftDiagonally(board, xCoord, yCoord)) {
+		if (isSquareEmpty(board, xCoord + 1, yCoord + 1)) {
+			allMoves.push_back(Move(xCoord, yCoord, xCoord + 1, yCoord + 1));
+		}
+	}
+
+	if (canMoveDownRightDiagonally(board, xCoord, yCoord)) {
+		if (isSquareEmpty(board, xCoord + 1, yCoord - 1)) {
+			allMoves.push_back(Move(xCoord, yCoord, xCoord + 1, yCoord - 1));
+		}
+	}
+}
+
+
+bool King::isPieceKing(std::vector<std::vector<Piece*>>& board, int8_t xCoord, int8_t yCoord) {
+	return dynamic_cast<King*>(board[xCoord][yCoord]) != nullptr;
+}
+
+bool King::canMoveUp(std::vector<std::vector<Piece*>>& board, int8_t xCoord) {
+	return xCoord > 0;
+}
+
+bool King::isSquareEmpty(std::vector<std::vector<Piece*>>& board, int8_t xCoord, int8_t yCoord) {
+	if (this -> isWhite) {
+		return (dynamic_cast<Piece*> (board[xCoord][yCoord]) == nullptr) || !(board[xCoord][yCoord] -> getIsWhite());
+	} else {
+		return (dynamic_cast<Piece*> (board[xCoord][yCoord]) == nullptr) || (board[xCoord][yCoord] -> getIsWhite());
+	}
+}
+
+bool King::canMoveUpRightDiagonally(std::vector<std::vector<Piece*>>& board, int8_t xCoord, int8_t yCoord) {
+	return xCoord > 0 && yCoord > 0;
+}
+
+bool King::canMoveUpLeftDiagonally(std::vector<std::vector<Piece*>>& board, int8_t xCoord, int8_t yCoord) {
+	return xCoord > 0 && yCoord < 7;
+}
+
+bool King::canMoveRight(std::vector<std::vector<Piece*>>& board, int8_t yCoord) {
+	return yCoord > 0;
+}
+
+bool King::canMoveLeft(std::vector<std::vector<Piece*>>& board, int8_t yCoord) {
+	return yCoord < 7;
+}
+
+bool King::canMoveDown(std::vector<std::vector<Piece*>>& board, int8_t xCoord) {
+	return xCoord < 7;
+}
+
+bool King::canMoveDownLeftDiagonally(std::vector<std::vector<Piece*>>& board, int8_t xCoord, int8_t yCoord) {
+	return xCoord < 7 && yCoord < 7;
+}
+
+bool King::canMoveDownRightDiagonally(std::vector<std::vector<Piece*>>& board, int8_t xCoord, int8_t yCoord) {
+	return xCoord < 7 && yCoord > 0;
+}
--- ChessGame/src/classes/Pieces/King/King.h
+++ ChessGame/src/classes/Pieces/King/King.h
@@ -8,4 +8,24 @@ class King : public Piece {
 		~King();
 
 		void getPieceLegalMoves(std::vector<std::vector<Piece*>>& board, std::vector<Move>& allMoves, int8_t xCoord, int8_t yCoord) override;
+
+	private:
+		bool isPieceKing(std::vector<std::vector<Piece*>>& board, int8_t xCoord, int8_t yCoord);
+
+		bool canMoveUp(std::vector<std::vector<Piece*>>& board, int8_t xCoord);
+		bool isSquareEmpty(std::vector<std::vector<Piece*>>& board, int8_t xCoord, int8_t yCoord);
+
+		bool canMoveUpRightDiagonally(std::vector<std::vector<Piece*>>& board, int8_t xCoord, int8_t yCoord);
+
+		bool canMoveUpLeftDiagonally(std::vector<std::vector<Piece*>>& board, int8_t xCoord, int8_t yCoord);
+
+		bool canMoveRight(std::vector<std::vector<Piece*>>& board, int8_t yCoord);
+
+		bool canMoveLeft(std::vector<std::vector<Piece*>>& board, int8_t yCoord);
+
+		bool canMoveDown(std::vector<std::vector<Piece*>>& board, int8_t xCoord);
+
+		bool canMoveDownLeftDiagonally(std::vector<std::vector<Piece*>>& board, int8_t xCoord, int8_t yCoord);
+
+		bool canMoveDownRightDiagonally(std::vector<std::vector<Piece*>>& board, int8_t xCoord, int8_t yCoord);
 };
