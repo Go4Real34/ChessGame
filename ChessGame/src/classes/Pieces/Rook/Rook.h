fff59235f9c10afbbbd7ca31508bd486f8a4af36
Go4Real34 <gorkemsarikaya2017@gmail.com>
2024-01-05 15:07:44 +0300

    add Rook class initialization function declarations

--- ChessGame/src/classes/Pieces/Rook/Rook.h
+++ ChessGame/src/classes/Pieces/Rook/Rook.h
@@ -3,4 +3,9 @@
 #include "../Piece.h"
 
 class Rook : public Piece {
+	public:
+		Rook(bool isWhite, int8_t XCoord, int8_t YCoord);
+		~Rook();
+
+		void getPieceLegalMoves(std::vector<std::vector<Piece*>>& board, std::vector<Move>& allMoves, int8_t xCoord, int8_t yCoord) override;
 };
