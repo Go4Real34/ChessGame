ecfa323613091f4e4f748a2d5dfb34bf499c0992
Go4Real34 <gorkemsarikaya2017@gmail.com>
2024-01-05 15:15:20 +0300

    add Queen class initialization function declarations

--- ChessGame/src/classes/Pieces/Queen/Queen.h
+++ ChessGame/src/classes/Pieces/Queen/Queen.h
@@ -3,4 +3,9 @@
 #include "../Piece.h"
 
 class Queen : public Piece {
+	public:
+		Queen(bool isWhite, int8_t XCoord, int8_t YCoord);
+		~Queen();
+
+		void getPieceLegalMoves(std::vector<std::vector<Piece*>>& board, std::vector<Move>& allMoves, int8_t xCoord, int8_t yCoord) override;
 };
