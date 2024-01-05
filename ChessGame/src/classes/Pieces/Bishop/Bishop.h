1a80a4cd914844503f362abe5baab9a20250ff0a
Go4Real34 <gorkemsarikaya2017@gmail.com>
2024-01-05 14:13:07 +0300

    add Bishop class initialization function declarations

--- ChessGame/src/classes/Pieces/Bishop/Bishop.h
+++ ChessGame/src/classes/Pieces/Bishop/Bishop.h
@@ -3,4 +3,9 @@
 #include "../Piece.h"
 
 class Bishop : public Piece {
+	public:
+		Bishop(bool isWhite, int8_t XCoord, int8_t YCoord);
+		~Bishop();
+
+		void getPieceLegalMoves(std::vector<std::vector<Piece*>>& board, std::vector<Move>& allMoves, int8_t xCoord, int8_t yCoord) override;
 };
