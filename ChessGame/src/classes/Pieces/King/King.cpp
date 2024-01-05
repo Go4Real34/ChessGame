0855d2c5ded1d3d17951d40eddc8fcb4d7fbd003
Go4Real34 <gorkemsarikaya2017@gmail.com>
2024-01-05 15:27:00 +0300

    add King class initialization function implementations

--- ChessGame/src/classes/Pieces/King/King.cpp
+++ ChessGame/src/classes/Pieces/King/King.cpp
@@ -1,1 +1,21 @@
+#include <SFML/Graphics.hpp>
+
 #include "King.h"
+
+King::King(bool isWhite, int8_t XCoord, int8_t YCoord) {
+	this -> isWhite = isWhite;
+	this -> xCoord = XCoord;
+	this -> yCoord = YCoord;
+
+	sf::Texture texture;
+	sf::IntRect blankPieceBackground;
+	if (this -> isWhite) {
+		texture.loadFromFile("./assets/textures/white_king.png", blankPieceBackground);
+	} else {
+		texture.loadFromFile("./assets/textures/black_king.png", blankPieceBackground);
+	}
+	this -> sprite.setTexture(texture, true);
+}
+
+King::~King() {
+}
